cd66b6cd6700bd09e2ac3f80999ead0c0ed0ea73
ptums <ptumulty923@gmail.com>
2024-11-24 10:00:47 -0500

    working on simplifying docker setup

--- setup.sh
+++ setup.sh
@@ -14,18 +14,6 @@ sudo apt install docker-ce -y
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-# Clone your project repository
-git clone https://github.com/ptums/nb-weather-project/tree/main
-cd your-repo
-
 # Build and run the Docker containers
 sudo docker-compose up -d
 
-# Install Certbot for SSL
-sudo apt install certbot python3-certbot-nginx -y
-
-# Obtain SSL certificate (replace yourdomain.com with your actual domain)
-sudo certbot --nginx -d newbrunswicknjhistoricalweather.com
-
-# Restart Nginx to apply SSL changes
-sudo docker-compose restart nginx
