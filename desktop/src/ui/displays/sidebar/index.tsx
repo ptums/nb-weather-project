d95ffeca48ec25013673546ac7f903b50734d867
ptums <ptumulty923@gmail.com>
2024-11-25 22:04:33 -0500

    Made a clone of client into desktop

---
+++ desktop/src/ui/displays/sidebar/index.tsx
@@ -0,0 +1,116 @@
+import { CompareWeatherData, WeatherQueries } from "../../../utils/types";
+import classNames from "classnames";
+
+import { RouteButton } from "./RoutePopover";
+import React, { useCallback } from "react";
+import { searchWeatherQueries, findWeatherDataById } from "../../../utils/api";
+import { convertToMonthsYears } from "../../../utils";
+
+interface SidebarProps {
+  title: string;
+  displayShadow?: boolean;
+  items: WeatherQueries[] | CompareWeatherData[];
+  setQuery: (query: string) => void;
+  setToggleView: (toggleView: boolean) => void;
+  setCompareList: (compareList: CompareWeatherData[]) => void;
+  handleDelete: () => void;
+}
+
+export const Sidebar = ({
+  title,
+  items,
+  displayShadow = false,
+  setQuery,
+  setToggleView,
+  setCompareList,
+  handleDelete,
+}: SidebarProps) => {
+  const addToCompareList = useCallback(
+    async (query: string) => {
+      const month = convertToMonthsYears(query, 0);
+      const year = convertToMonthsYears(query, 1);
+
+      const results = await searchWeatherQueries(query);
+
+      if (results.length > 0) {
+        const weatherData = await findWeatherDataById(results[0]?.id);
+
+        const newItem = {
+          weatherData,
+          month,
+          year,
+          id: Date.now().toString(),
+          query,
+        };
+
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        //@ts-expect-error
+        setCompareList((prevList) => [...prevList, newItem]);
+      }
+    },
+    [setCompareList]
+  );
+
+  const removeFromCompareList = useCallback(
+    (id: string) => {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      //@ts-expect-error
+      setCompareList((prevList) => prevList.filter((item) => item.id !== id));
+    },
+    [setCompareList]
+  );
+
+  return (
+    <div
+      className={classNames("h-screen w-2/12 border-r bg-white", {
+        "border-silver": displayShadow,
+      })}
+    >
+      <div className="p-4 relative h-full">
+        <h2>
+          <button
+            onClick={() => handleDelete()}
+            className="text-lg tracking-tight font-semibold text-rose-900 mb-4"
+          >
+            {title}
+          </button>
+        </h2>
+        <ul className="space-y-4">
+          {items &&
+            items
+              .sort((a, b) => (a?.id < b?.id ? 1 : -1))
+              .map((item, index) => {
+                return (
+                  <React.Fragment key={index}>
+                    {title.includes("History") ? (
+                      <li className="flex items-center space-x-2 text-sm uppercase tracking-tight font-semibold text-rose-600 cursor-pointer hover:text-rose-900 bg-transparent">
+                        <RouteButton
+                          onVisualize={() => {
+                            setQuery(item.query);
+                            setToggleView(true);
+                          }}
+                          onCompare={() => {
+                            addToCompareList(item.query);
+                            setToggleView(false);
+                          }}
+                          label={item.query}
+                        />
+                      </li>
+                    ) : (
+                      <li
+                        onClick={() =>
+                          removeFromCompareList(item.id.toString() as string)
+                        }
+                        className="flex items-center space-x-2 text-sm tracking-tight font-semibold text-rose-600 cursor-pointer hover:text-rose-900"
+                      >
+                        <span>{item.query}</span>
+                      </li>
+                    )}
+                  </React.Fragment>
+                );
+              })}
+        </ul>
+      </div>
+    </div>
+  );
+};
---
+++ desktop/src/utils/api.ts
@@ -0,0 +1,117 @@
+import { WEAHTER_DATA_ENDPOINT, WEAHTER_QUERY_ENDPOINT } from "./constants";
+import { PayloadTypes } from "./types";
+
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const buildApiUrl = (path: string) => `${API_BASE_URL}${path}`;
+
+/* Weather Queries requests */
+export async function createWeatherQueries(query: string, userId: string) {
+  const url = buildApiUrl(WEAHTER_QUERY_ENDPOINT);
+  const payload = { query, userId };
+  const data = await postResponse(url, payload);
+
+  return data;
+}
+
+export async function findWeatherQueriesById(queryId: number) {
+  const path = `${WEAHTER_QUERY_ENDPOINT}/i/${queryId}`;
+  const url = buildApiUrl(path);
+
+  const data = getResponse(url);
+
+  return [...(await data)];
+}
+
+export async function searchWeatherQueries(query: string) {
+  const path = `${WEAHTER_QUERY_ENDPOINT}/s?query=${query}`;
+  const url = buildApiUrl(path);
+
+  const data = await getResponse(url);
+
+  return [...data];
+}
+
+export async function findWeatherQueriesByUserId(userId: string) {
+  const path = `${WEAHTER_QUERY_ENDPOINT}/u/${userId}`;
+  const url = buildApiUrl(path);
+
+  const data = getResponse(url);
+
+  return [...(await data)];
+}
+
+export async function deleteWeatherQueries(queryId: number) {
+  const path = `${WEAHTER_QUERY_ENDPOINT}/i/${queryId}`;
+  const url = buildApiUrl(path);
+
+  const data = deleteResponse(url);
+
+  return [...(await data)];
+}
+
+/* Weather Data requests */
+export async function createWeatherData(query: string, queryId: number) {
+  const url = buildApiUrl(WEAHTER_DATA_ENDPOINT);
+
+  const payload = { query, queryId };
+  const data = postResponse(url, payload);
+
+  return [...(await data)];
+}
+
+export async function findWeatherDataById(queryId: number) {
+  const path = `${WEAHTER_DATA_ENDPOINT}/i/${queryId}`;
+  const url = buildApiUrl(path);
+
+  const data = await getResponse(url);
+
+  return [...data];
+}
+
+/** HELPERS  */
+async function getResponse(url: string) {
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  const data = await response.json();
+
+  return data;
+}
+
+async function postResponse(url: string, payload: PayloadTypes) {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  const data = await response.json();
+
+  return data;
+}
+
+async function deleteResponse(url: string) {
+  const response = await fetch(url, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  const data = await response.json();
+
+  return data;
+}
